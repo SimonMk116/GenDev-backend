b8dee685c2c88d78698e63bb6ba3a6c9fa170969
Simon Thomas Malik <ge64dek@mytum.de>
2025-05-20 15:33:40 +0200

    crossorigin

--- src/main/java/com/SimonMk116/gendev/controller/UserDataController.java
+++ src/main/java/com/SimonMk116/gendev/controller/UserDataController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/user-activity")
 @CrossOrigin(origins = {
         "https://6000-firebase-studio-1747694501106.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev",
+        "https://6000-firebase-studio-1747694501106.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev/api/user-activity/log-search",
         "http://localhost:9002",
         "http://localhost:3001",
         "https://studio.firebase.google.com"},
